7a417eefad6c8ad2407cb4f8a560acb94486bf2a
Yogesh Karande <yogeshkarande640@gmail.com>
2024-11-17 21:11:19 +0530

    update app.js

--- app.js
+++ app.js
@@ -13,22 +13,22 @@ const { PORT } = process.env;
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log("listening port", PORT);
-});
-
-// app.use(express.urlencoded({ extended: false }));
+// routes
 todoRoutes(app);
 userRoutes(app);
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
 app.use(function (req, res) {
   res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+app.listen(PORT, () => {
+  console.log("listening port", PORT);
 });
 
 module.exports = app;
