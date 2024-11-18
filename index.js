0a07a20c41d334c5711e5fea9f6c768297c36f9d
Yogesh Karande <yogeshkarande640@gmail.com>
2024-11-18 21:13:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const todoRoutes = require("./routes/todo");
 const Task = require("./models/todo");
 
 const port = process.env.PORT || 7000;
-// const origin = process.env.FRONTEND_ORIGIN;
+const origin = process.env.FRONTEND_ORIGIN;
 
 const app = express();
 
 // Middleware
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: [origin],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: [`${origin}`],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
